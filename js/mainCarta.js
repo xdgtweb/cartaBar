340d2f3894c312573d5ea51ef3d431aff28ffc86
xdgtweb <110548730+xdgtweb@users.noreply.github.com>
2023-09-22 15:37:58 +0200

    x

--- js/mainCarta.js
+++ js/mainCarta.js
@@ -1,8 +1,8 @@
 const mainCartaHTML = `
 
     <div class="centrado">
-      <img class="fondo" src="./img/index/2.jpg">
-      <img src="./img/index/2.jpg">
+      <img class="fondo" src="./img/carta/carta1.jpg">
+      <img src="./img/carta/carta1.jpg">
     </div>
 
 `;
--- js/mainContacto.js
+++ js/mainContacto.js
@@ -4,5 +4,10 @@ const mainContactoHTML = `
       <img class="fondo" src="./img/index/3.jpg">
       <img src="./img/index/3.jpg">
     </div>
+    <div class="centrado">
+      <a href="">
+        <img src="./img/contacto/whatsapp.png">
+      </a>
+    </div>
 
 `;
